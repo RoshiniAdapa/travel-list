fffc9cc448cac1ae4c0c223868f4061c962fec19
Roshini Adapa <roshiniadapa@gmail.com>
2024-09-12 01:09:12 +0530

    travel-list | controlled elements

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -20,10 +22,42 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = React.useState("");
+  const [quantity, setQuantity] = React.useState(5);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {" "}
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -31,7 +65,7 @@ function PackingList() {
   return (
     <ul className="list">
       {initialItems.map((item) => (
-        <Item item={item} />
+        <Item item={item} key={item.id} />
       ))}
     </ul>
   );
